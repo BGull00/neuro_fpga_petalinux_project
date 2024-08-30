75016b106060e8945488ac503214f1e33e45551f
Bryson Gullett <brysongullett@gmail.com>
2024-08-30 21:09:40 +0000

    Updated petalinux build and copy to sd card script

--- build-copy.sh
+++ build-copy.sh
@@ -27,15 +27,22 @@ cp ../vivado/pynq_dma_test.runs/impl_1/design_1_wrapper.bit network.bit
 bootgen -image Full_Bitstream.bif -arch zynq -process_bitstream bin
 sudo cp network.bit.bin /media/bryson/dd8edc57-7308-476f-b90a-4f3dd6776c58/lib/firmware/xilinx/pynq-dma-test/
 
-# Clone fresh TENNLab framework to SD card (without .git/)
-rm -rf framework
-git clone -b zynq_dma git@bitbucket.org:neuromorphic-utk/framework.git
+# Copy updated framework to SD card
+# rm -rf framework
+# git clone -b zynq_dma git@bitbucket.org:neuromorphic-utk/framework.git
 cd framework
+git pull
+# rm -rf /media/bryson/dd8edc57-7308-476f-b90a-4f3dd6776c58/home/petalinux/framework
 mkdir /media/bryson/dd8edc57-7308-476f-b90a-4f3dd6776c58/home/petalinux/framework
 git archive HEAD | tar -x -C /media/bryson/dd8edc57-7308-476f-b90a-4f3dd6776c58/home/petalinux/framework
+rm -f /media/bryson/dd8edc57-7308-476f-b90a-4f3dd6776c58/home/petalinux/framework/processors/zynq_dma/bin/*
+rm -f /media/bryson/dd8edc57-7308-476f-b90a-4f3dd6776c58/home/petalinux/framework/processors/zynq_dma/obj/*
+rm -f /media/bryson/dd8edc57-7308-476f-b90a-4f3dd6776c58/home/petalinux/framework/processors/zynq_dma/build/*.*
+rm -f /media/bryson/dd8edc57-7308-476f-b90a-4f3dd6776c58/home/petalinux/framework/processors/zynq_dma/build/bindings/*
+rm -f /media/bryson/dd8edc57-7308-476f-b90a-4f3dd6776c58/home/petalinux/framework/processors/zynq_dma/lib/*.a
 
 # Copy xor network to SD card
-cp ~/Documents/TENNLab/framework/cpp-apps/networks/xor_noleak.txt /media/bryson/dd8edc57-7308-476f-b90a-4f3dd6776c58/home/petalinux/framework/cpp-apps/networks/
+cp ~/Documents/TENNLab/framework/cpp-apps/networks/xor_noleak_dma.txt /media/bryson/dd8edc57-7308-476f-b90a-4f3dd6776c58/home/petalinux/framework/cpp-apps/networks/
 
 # Open putty to connect to Zynq serial terminal
 sudo putty
